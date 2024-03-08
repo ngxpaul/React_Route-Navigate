67b2e83ff8543688443088c92f214e4a3ff134b4
ngxpaul <phongnx.22git@vku.udn.vn>
2024-03-08 13:12:40 +0700

    fix Show Error page

--- src/page/ErrorPage.js
+++ src/page/ErrorPage.js
@@ -1,7 +1,13 @@
+import MainNavigation from '../components/MainNavigation';
 function ErrorPage(){
     return (
         <div>
+            <MainNavigation/>
+            <main>
             <h1>Error occured here</h1>
+            <p>Could not find this page</p>
+            </main>
+
         </div>
     );
 }
